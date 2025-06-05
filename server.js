02441f2b65cd5b6ed27bb8d25922c7940f57db28
Ilkin Ibadov <ilkin.ibadov21@gmail.com>
2025-06-05 07:17:28 +0400

    cors bug fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://localhost:3000',
   credentials: true
 }));
 const server = http.createServer(app);
